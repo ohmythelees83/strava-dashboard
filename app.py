7282a9bbaef4fcced73f2c894507545ff95ad643
ohmythelees83 <alexlee993@yahoo.co.uk>
2025-04-08 16:20:09 +0100

    Updating mileage chart to show only last 10 weeks of running

--- app.py
+++ app.py
@@ -154,6 +154,8 @@ ax.plot(
     weekly_mileage_trimmed["Total Miles"],
     marker='o',
     label='Weekly Mileage'
+    ax.axhline(y=20, color='red', linestyle='--', label='20mi Goal')
+
 )
 ax.set_title("Weekly Running Mileage - Last 10 Weeks")
 ax.set_xlabel("Week Starting")
